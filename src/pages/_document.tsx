cd9a76f8922f698bda76a611fbbd5901b00e27d8
Iago Virgílio <iagovirgilio@gmail.com>
2024-09-03 14:43:26 -0300

    Corrigir title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <title>NFT Crypto</title>
-      <meta name="description" content="NFT Crypto" />
+      <title>Crypto</title>
+      <meta name="description" content="Crypto" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <Head />
