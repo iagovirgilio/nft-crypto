7b62bbbb5a0e41fac3c7586055b962bd112f049e
Iago Virgílio <iagovirgilio@gmail.com>
2024-09-03 11:53:03 -0300

    Adicoinar title e meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
+      <title>NFT Crypto</title>
+      <meta name="description" content="NFT Crypto" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
       <Head />
       <body>
         <Main />
