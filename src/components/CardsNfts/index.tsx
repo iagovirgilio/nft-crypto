9e07a5d299f67f7dce84a8f4d16b0296c4ab99aa
Iago Virgílio <iagovirgilio@gmail.com>
2024-09-07 17:40:57 -0300

    Criar cards NFts

--- src/components/CardsNfts/index.tsx
+++ src/components/CardsNfts/index.tsx
@@ -37,6 +37,78 @@ const CardsNfts = () => {
                             </div>
                         </div>
                     </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardImage}>
+                            <img src="CardNFT02.png" alt="" />
+                        </div>
+                        <div className={styles.cardInfo}>
+                            <div className={styles.bottomLeft}>
+                                <img src="/Avatar2.png" alt="Avatar Owner" />
+                                <div className={styles.owner}>
+                                    <span>Owned by</span>
+                                    <h3>Hacker</h3>
+                                </div>
+                            </div>
+                            <div className={styles.bottomRight}>
+                                <small>2.432,21$</small>
+                                <span>1.2 ETH</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardImage}>
+                            <img src="CardNFT03.png" alt="" />
+                        </div>
+                        <div className={styles.cardInfo}>
+                            <div className={styles.bottomLeft}>
+                                <img src="/Avatar3.png" alt="Avatar Owner" />
+                                <div className={styles.owner}>
+                                    <span>Owned by</span>
+                                    <h3>Hacker</h3>
+                                </div>
+                            </div>
+                            <div className={styles.bottomRight}>
+                                <small>2.432,21$</small>
+                                <span>1.2 ETH</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardImage}>
+                            <img src="CardNFT04.png" alt="" />
+                        </div>
+                        <div className={styles.cardInfo}>
+                            <div className={styles.bottomLeft}>
+                                <img src="/Avatar4.png" alt="Avatar Owner" />
+                                <div className={styles.owner}>
+                                    <span>Owned by</span>
+                                    <h3>Hacker</h3>
+                                </div>
+                            </div>
+                            <div className={styles.bottomRight}>
+                                <small>2.432,21$</small>
+                                <span>1.2 ETH</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardImage}>
+                            <img src="CardNFT01.png" alt="" />
+                        </div>
+                        <div className={styles.cardInfo}>
+                            <div className={styles.bottomLeft}>
+                                <img src="/Avatar1.png" alt="Avatar Owner" />
+                                <div className={styles.owner}>
+                                    <span>Owned by</span>
+                                    <h3>Hacker</h3>
+                                </div>
+                            </div>
+                            <div className={styles.bottomRight}>
+                                <small>2.432,21$</small>
+                                <span>1.2 ETH</span>
+                            </div>
+                        </div>
+                    </div>
     
                 </div>
             </div>
