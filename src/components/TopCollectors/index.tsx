bc94cb2dead819875e8282c7fd270343b8fdd211
Iago Virgílio <iagovirgilio@gmail.com>
2024-09-07 23:06:35 -0300

    Concluir rank

--- src/components/TopCollectors/index.tsx
+++ src/components/TopCollectors/index.tsx
@@ -4,6 +4,174 @@ const TopCollectors = () => {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
+                <div className={styles.topBar}>
+                    <div className={styles.leftSide}>
+                        <span>This week</span>
+                        <h3>Top Collectors</h3>
+                    </div>
+                    <div className={styles.rightSide}>
+                        <div className={styles.dropdown}>
+                            <b className={styles.all}>This week</b>
+                            <img className={styles.linedownIcon} alt="" src="downw.png" />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.topCollectors}>
+                    <div className={styles.rank}>
+                        <div className={styles.avatarhandle}>
+                            <div className={styles.avatar}>
+                                <img className={styles.avatar10Icon} alt="" src="Avatar/10.png" />
+                                <img className={styles.outrosverificadoIcon} alt="" src="Outros/verificado.svg" />
+                            </div>
+                            <div className={styles.name}>
+                                <b className={styles.jhon}>Jhon</b>
+                                <div className={styles.jhonnft}>@jhonNFT</div>
+                            </div>
+                        </div>
+                        <div className={styles.value}>
+                            <div className={styles.eth}>
+                                <b className={styles.jhon}>32.322,2 ETH</b>
+                                <img className={styles.linecryptoIcon} alt="" src="minicrypto.svg" />
+                            </div>
+                            <div className={styles.div}>
+                                <img className={styles.linedownIcon} alt="" src="down.svg" />
+                                <b className={styles.b}>20%</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.rank}>
+                        <div className={styles.avatarhandle}>
+                            <div className={styles.avatar}>
+                                <img className={styles.avatar10Icon} alt="" src="Avatar/10.png" />
+                                <img className={styles.outrosverificadoIcon} alt="" src="Outros/verificado.svg" />
+                            </div>
+                            <div className={styles.name}>
+                                <b className={styles.jhon}>Jhon</b>
+                                <div className={styles.jhonnft}>@jhonNFT</div>
+                            </div>
+                        </div>
+                        <div className={styles.value}>
+                            <div className={styles.eth}>
+                                <b className={styles.jhon}>32.322,2 ETH</b>
+                                <img className={styles.linecryptoIcon} alt="" src="minicrypto.svg" />
+                            </div>
+                            <div className={styles.div}>
+                                <img className={styles.linedownIcon} alt="" src="down.svg" />
+                                <b className={styles.b}>20%</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.rank}>
+                        <div className={styles.avatarhandle}>
+                            <div className={styles.avatar}>
+                                <img className={styles.avatar10Icon} alt="" src="Avatar/10.png" />
+                                <img className={styles.outrosverificadoIcon} alt="" src="Outros/verificado.svg" />
+                            </div>
+                            <div className={styles.name}>
+                                <b className={styles.jhon}>Jhon</b>
+                                <div className={styles.jhonnft}>@jhonNFT</div>
+                            </div>
+                        </div>
+                        <div className={styles.value}>
+                            <div className={styles.eth}>
+                                <b className={styles.jhon}>32.322,2 ETH</b>
+                                <img className={styles.linecryptoIcon} alt="" src="minicrypto.svg" />
+                            </div>
+                            <div className={styles.div}>
+                                <img className={styles.linedownIcon} alt="" src="down.svg" />
+                                <b className={styles.b}>20%</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.rank}>
+                        <div className={styles.avatarhandle}>
+                            <div className={styles.avatar}>
+                                <img className={styles.avatar10Icon} alt="" src="Avatar/10.png" />
+                                <img className={styles.outrosverificadoIcon} alt="" src="Outros/verificado.svg" />
+                            </div>
+                            <div className={styles.name}>
+                                <b className={styles.jhon}>Jhon</b>
+                                <div className={styles.jhonnft}>@jhonNFT</div>
+                            </div>
+                        </div>
+                        <div className={styles.value}>
+                            <div className={styles.eth}>
+                                <b className={styles.jhon}>32.322,2 ETH</b>
+                                <img className={styles.linecryptoIcon} alt="" src="minicrypto.svg" />
+                            </div>
+                            <div className={styles.div}>
+                                <img className={styles.linedownIcon} alt="" src="down.svg" />
+                                <b className={styles.b}>20%</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.rank}>
+                        <div className={styles.avatarhandle}>
+                            <div className={styles.avatar}>
+                                <img className={styles.avatar10Icon} alt="" src="Avatar/10.png" />
+                                <img className={styles.outrosverificadoIcon} alt="" src="Outros/verificado.svg" />
+                            </div>
+                            <div className={styles.name}>
+                                <b className={styles.jhon}>Jhon</b>
+                                <div className={styles.jhonnft}>@jhonNFT</div>
+                            </div>
+                        </div>
+                        <div className={styles.value}>
+                            <div className={styles.eth}>
+                                <b className={styles.jhon}>32.322,2 ETH</b>
+                                <img className={styles.linecryptoIcon} alt="" src="minicrypto.svg" />
+                            </div>
+                            <div className={styles.div}>
+                                <img className={styles.linedownIcon} alt="" src="down.svg" />
+                                <b className={styles.b}>20%</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.rank}>
+                        <div className={styles.avatarhandle}>
+                            <div className={styles.avatar}>
+                                <img className={styles.avatar10Icon} alt="" src="Avatar/10.png" />
+                                <img className={styles.outrosverificadoIcon} alt="" src="Outros/verificado.svg" />
+                            </div>
+                            <div className={styles.name}>
+                                <b className={styles.jhon}>Jhon</b>
+                                <div className={styles.jhonnft}>@jhonNFT</div>
+                            </div>
+                        </div>
+                        <div className={styles.value}>
+                            <div className={styles.eth}>
+                                <b className={styles.jhon}>32.322,2 ETH</b>
+                                <img className={styles.linecryptoIcon} alt="" src="minicrypto.svg" />
+                            </div>
+                            <div className={styles.div}>
+                                <img className={styles.linedownIcon} alt="" src="down.svg" />
+                                <b className={styles.b}>20%</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.rank}>
+                        <div className={styles.avatarhandle}>
+                            <div className={styles.avatar}>
+                                <img className={styles.avatar10Icon} alt="" src="Avatar/10.png" />
+                                <img className={styles.outrosverificadoIcon} alt="" src="Outros/verificado.svg" />
+                            </div>
+                            <div className={styles.name}>
+                                <b className={styles.jhon}>Jhon</b>
+                                <div className={styles.jhonnft}>@jhonNFT</div>
+                            </div>
+                        </div>
+                        <div className={styles.value}>
+                            <div className={styles.eth}>
+                                <b className={styles.jhon}>32.322,2 ETH</b>
+                                <img className={styles.linecryptoIcon} alt="" src="minicrypto.svg" />
+                            </div>
+                            <div className={styles.div}>
+                                <img className={styles.linedownIcon} alt="" src="down.svg" />
+                                <b className={styles.b}>20%</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
