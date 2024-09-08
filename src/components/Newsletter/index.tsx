62450f30bf49ed3557cc822003f47e8f9d739bae
Iago Virgílio <iagovirgilio@gmail.com>
2024-09-08 11:19:46 -0300

    Adicinar newsletter

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,12 +1,29 @@
-import styles from './styles.module.scss'
+import type { NextPage } from 'next';
+import styles from "./styles.module.scss";
 
-const Newsletter = () => {
+
+const Newsletter: NextPage = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.content}>
+            <div className={styles.newsletter}>
+                <img className={styles.maskGroupIcon} alt="" src="./Mask group.png" />
+                <form className={styles.formNewsletter}>
+                    <div className={styles.subscribeToOur }>Subscribe to Our Newsletter</div>
+                    <div className={styles.text2Newsletter }>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                    <div className={styles.inputGroup}>
+                        <input
+                            className={styles.inputNewsletter}
+                            type="text"
+                            placeholder="Type your email"
+                        />
+                        <button className={styles.buttonNewsletter} type="submit">
+                            Subscribe
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Newsletter
+export default Newsletter;
